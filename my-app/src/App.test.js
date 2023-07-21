2e03688ff809c4108a282fa617492b19a4debdab
sjhu2020 <sjhu@uw.edu>
2023-07-20 17:54:17 -0700

    add tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //userEvent.click.(screen.getByTestId("checkbox"));
+  expect(screen.getByTestId("checkbox")).not.toBeChecked(); //npm run test
+});
+
+test('check that there is a enter button', () => {
+  render(<App />);
+  const button = screen.getByRole('button', {
+    name: /enter/i
+  });
+  expect(button).toBeInTheDocument();
 });
